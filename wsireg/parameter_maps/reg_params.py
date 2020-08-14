5a0303d20a983d564c5c5322bf5c642d8670856b
nhpatterson <nheathpatterson@gmail.com>
2020-08-14 15:49:41 -0500

    make default affine transform affine

--- wsireg/parameter_maps/reg_params.py
+++ wsireg/parameter_maps/reg_params.py
@@ -90,7 +90,7 @@ DEFAULT_REG_PARAM_MAPS = {
         "Resampler": ['DefaultResampler'],
         "ResultImageFormat": ['mha'],
         "ResultImagePixelType": ['short'],
-        "Transform": ['EulerTransform'],
+        "Transform": ['AdvancedAffineTransform'],
         "UseDirectionCosines": ['true'],
         "WriteResultImage": ['false'],
         "WriteTransformParametersEachResolution": ['true'],
