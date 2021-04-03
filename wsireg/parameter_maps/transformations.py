cdcacde16da3723a3a7ad106b529b6db07cdb68b
nhpatterson <nheathpatterson@gmail.com>
2021-04-02 19:47:47 -0500

    Add a base translation transform

--- wsireg/parameter_maps/transformations.py
+++ wsireg/parameter_maps/transformations.py
@@ -30,6 +30,39 @@ BASE_RIG_TFORM = dict(
     }
 )
 
+BASE_TRANSLATION_TFORM = dict(
+    {
+        "Transform": ["TranslationTransform"],
+        "NumberOfParameters": ["2"],
+        "TransformParameters": ["0", "0"],
+        "InitialTransformParametersFileName": ["NoInitialTransform"],
+        "HowToCombineTransforms": ["Compose"],
+        "FixedImageDimension": ["2"],
+        "MovingImageDimension": ["2"],
+        "FixedInternalImagePixelType": ["float"],
+        "MovingInternalImagePixelType": ["float"],
+        "Size": ["0", "0"],
+        "Index": ["0", "0"],
+        "Spacing": ["", ""],
+        "Origin": ["0.0000", "0.0000"],
+        "Direction": [
+            "1.0000000000",
+            "0.0000000000",
+            "0.0000000000",
+            "1.0000000000",
+        ],
+        "UseDirectionCosines": ["true"],
+        "CenterOfRotationPoint": ["0", "0"],
+        "ResampleInterpolator": ["FinalNearestNeighborInterpolator"],
+        "Resampler": ["DefaultResampler"],
+        "DefaultPixelValue": ["0.000000"],
+        "ResultImageFormat": ["mha"],
+        "ResultImagePixelType": ["float"],
+        "CompressResultImage": ["true"],
+    }
+)
+
+
 BASE_AFF_TFORM = dict(
     {
         "Transform": ["AffineTransform"],
