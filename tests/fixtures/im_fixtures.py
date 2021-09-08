69de339988561be7d45ffee5be6d1ef8579bdb40
Heath Patterson <nheathpatterson@gmail.com>
2021-09-08 12:40:18 -0500

    add new pyramidal image fixtures

--- tests/fixtures/im_fixtures.py
+++ tests/fixtures/im_fixtures.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 import dask.array as da
 import zarr
-from tifffile import imwrite
+from tifffile import TiffWriter, imwrite, imread
 
 
 @pytest.fixture
@@ -70,19 +70,110 @@ def zarr_im_rgb_np():
 @pytest.fixture
 def disk_im_mch(tmpdir_factory, im_mch_np):
     out_im = tmpdir_factory.mktemp("image").join("image_fp_mch.tiff")
-    imwrite(out_im, im_mch_np)
+    imwrite(out_im, im_mch_np, tile=(256, 256))
     return out_im
 
+@pytest.fixture
+def disk_im_mch_notile(tmpdir_factory, im_mch_np):
+    out_im = tmpdir_factory.mktemp("image").join("image_fp_mch_nt.tiff")
+    imwrite(out_im, im_mch_np, photometric="MINISBLACK", tile=(256,256))
+    return out_im
 
 @pytest.fixture
 def disk_im_rgb(tmpdir_factory, im_rgb_np):
     out_im = tmpdir_factory.mktemp("image").join("image_fp_rgb.tiff")
-    imwrite(out_im, im_rgb_np)
+    imwrite(out_im, im_rgb_np, tile=(256, 256))
     return out_im
 
 
 @pytest.fixture
 def disk_im_gry(tmpdir_factory, im_gry_np):
     out_im = tmpdir_factory.mktemp("image").join("image_fp_gry.tiff")
-    imwrite(out_im, im_gry_np)
+    imwrite(out_im, im_gry_np, tile=(256, 256))
+    return out_im
+
+
+@pytest.fixture
+def disk_im_mch_pyr(tmpdir_factory):
+    out_im = tmpdir_factory.mktemp("image").join("image_fp_mch_pyr.tiff")
+    subifds = 2
+    full_im = np.random.randint(0, 255, (3, 2048, 2048), dtype=np.uint16)
+    with TiffWriter(out_im) as tif:
+        for ch in range(full_im.shape[0]):
+            options = dict(
+                tile=(256, 256),
+                compression="deflate",
+                photometric="minisblack",
+                metadata=None,
+            )
+            tif.write(
+                full_im[ch, :, :],
+                subifds=subifds,
+                **options,
+            )
+
+            for pyr_idx in range(subifds):
+                if pyr_idx == 0:
+                    subresimage = full_im[ch, ::2, ::2]
+                else:
+                    subresimage = subresimage[::2, ::2]
+
+                tif.write(subresimage, **options, subfiletype=1)
+    return out_im
+
+
+@pytest.fixture
+def disk_im_rgb_pyr(tmpdir_factory):
+    out_im = tmpdir_factory.mktemp("image").join("image_fp_rgb_pyr.tiff")
+    subifds = 2
+    full_im = np.random.randint(0, 255, (2048, 2048, 3), dtype=np.uint8)
+    with TiffWriter(out_im) as tif:
+        options = dict(
+            tile=(128, 128),
+            compression="deflate",
+            photometric="rgb",
+            metadata=None,
+        )
+        tif.write(
+            full_im,
+            subifds=subifds,
+            **options,
+        )
+
+        for pyr_idx in range(subifds):
+            if pyr_idx == 0:
+                subresimage = full_im[::2, ::2, :]
+            else:
+                subresimage = subresimage[::2, ::2, :]
+
+            tif.write(subresimage, **options, subfiletype=1)
+
+    return out_im
+
+
+@pytest.fixture
+def disk_im_gry_pyr(tmpdir_factory):
+    out_im = tmpdir_factory.mktemp("image").join("image_fp_gry_pyr.tiff")
+    subifds = 2
+    full_im = np.random.randint(0, 255, (2048, 2048), dtype=np.uint16)
+    with TiffWriter(out_im) as tif:
+        options = dict(
+            tile=(256, 256),
+            compression="deflate",
+            photometric="minisblack",
+            metadata=None,
+        )
+        tif.write(
+            full_im,
+            subifds=subifds,
+            **options,
+        )
+
+        for pyr_idx in range(subifds):
+            if pyr_idx == 0:
+                subresimage = full_im[::2, ::2]
+            else:
+                subresimage = subresimage[::2, ::2]
+
+            tif.write(subresimage, **options, subfiletype=1)
     return out_im
