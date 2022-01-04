a12df07e89bc5582c830d84c68c78f4af9882973
nhpatterson <nheathpatterson@gmail.com>
2022-01-04 13:23:18 -0600

    fix configs and config tests

--- tests/test_wsireg_config.py
+++ tests/test_wsireg_config.py
@@ -1,14 +1,14 @@
 import pytest
+import os
 from pathlib import Path
 from wsireg.wsireg2d import WsiReg2D
 from wsireg.utils.config_utils import parse_check_reg_config
 
 
-@pytest.fixture(scope="session")
-def data_out_dir(tmpdir_factory):
-    out_dir = tmpdir_factory.mktemp("output")
-    return out_dir
-
+HERE = os.path.dirname(__file__)
+FIXTURES_DIR = os.path.join(HERE, "fixtures")
+config1_fp = str(Path(FIXTURES_DIR) / "test-config1.yaml")
+config2_fp = str(Path(FIXTURES_DIR) / "test-config2.yaml")
 
 def config_to_WsiReg2D(config_filepath, output_dir):
     reg_config = parse_check_reg_config(config_filepath)
@@ -21,29 +21,29 @@ def config_to_WsiReg2D(config_filepath, output_dir):
     return reg_graph
 
 
-def test_wsireg_config1(data_out_dir):
-    wsi_reg = config_to_WsiReg2D("./fixtures/test-config1.yaml", data_out_dir)
-    wsi_reg.add_data_from_config("./fixtures/test-config1.yaml")
+@pytest.fixture(scope="session")
+def data_out_dir(tmpdir_factory):
+    out_dir = tmpdir_factory.mktemp("output")
+    return out_dir
+
+
+@pytest.mark.parametrize("config_fp", [(config1_fp), (config2_fp)])
+def test_wsireg_configs(config_fp, data_out_dir):
+    wsi_reg = config_to_WsiReg2D(config_fp, data_out_dir)
+    wsi_reg.add_data_from_config(config_fp)
     wsi_reg.register_images()
     wsi_reg.save_transformations()
     assert wsi_reg.output_dir == Path(str(data_out_dir))
 
 
-def test_wsireg_config2_chsel(data_out_dir):
-    wsi_reg = config_to_WsiReg2D("./fixtures/test-config2.yaml", data_out_dir)
-    wsi_reg.add_data_from_config("./fixtures/test-config2.yaml")
-    wsi_reg.register_images()
-    wsi_reg.save_transformations()
-    assert wsi_reg.output_dir == Path(str(data_out_dir))
-
-
-def test_wsireg_config1_fromcache(data_out_dir):
-    wsi_reg1 = config_to_WsiReg2D("./fixtures/test-config1.yaml", data_out_dir)
-    wsi_reg1.add_data_from_config("./fixtures/test-config1.yaml")
+@pytest.mark.parametrize("config_fp", [(config1_fp), (config2_fp)])
+def test_wsireg_configs_fromcache(config_fp, data_out_dir):
+    wsi_reg1 = config_to_WsiReg2D(config_fp, data_out_dir)
+    wsi_reg1.add_data_from_config(config_fp)
     wsi_reg1.register_images()
 
-    wsi_reg2 = config_to_WsiReg2D("./fixtures/test-config1.yaml", data_out_dir)
-    wsi_reg2.add_data_from_config("./fixtures/test-config1.yaml")
+    wsi_reg2 = config_to_WsiReg2D(config_fp, data_out_dir)
+    wsi_reg2.add_data_from_config(config_fp)
     wsi_reg2.register_images()
 
     assert wsi_reg1.output_dir == Path(str(data_out_dir))
