54e7c7ac803fc24fbf23a1cff4e3425387cb2ccd
nhp <nheathpatterson@gmail.com>
2022-02-28 12:53:55 -0600

    refactor and greatly improved tiled OME-TIFF writer

--- wsireg/writers/ome_tiff_writer.py
+++ wsireg/writers/ome_tiff_writer.py
@@ -13,16 +13,6 @@ from wsireg.utils.im_utils import (
     get_pyramid_info,
     prepare_ome_xml_str,
 )
-from wsireg.utils.tile_image_transform import (
-    add_tile_location_on_moving,
-    compute_sub_res,
-    determine_moving_tile_padding,
-    generate_tile_coords,
-    itk_transform_tiles,
-    subres_zarr_to_tiles,
-    tile_pad_output_size,
-    zarr_to_tiles,
-)
 
 
 class OmeTiffWriter:
@@ -53,7 +43,6 @@ class OmeTiffWriter:
         write_pyramid=True,
         tile_size=512,
         compression="default",
-        by_tile=False,
     ):
 
         if reg_transform_seq:
@@ -74,11 +63,6 @@ class OmeTiffWriter:
             or self.x_size / self.tile_size <= 1
         ):
             self.tile_size = self.tile_size // 2
-
-        if by_tile:
-            self.y_size, self.x_size = tile_pad_output_size(
-                self.y_size, self.x_size, tile_size=self.tile_size
-            )
 
         self.pyr_levels, _ = get_pyramid_info(
             self.y_size, self.x_size, self.reg_image.n_ch, self.tile_size
@@ -238,170 +222,4 @@ class OmeTiffWriter:
                             cv2.INTER_LINEAR,
                         )
                         tif.write(rgb_im_data, **options, subfiletype=1)
-        return output_file_name
-
-    def write_image_by_tile(
-        self,
-        image_name,
-        output_dir="",
-        write_pyramid=True,
-        tile_size=512,
-        compression="default",
-        tile_padding=None,
-        use_multiprocessing=False,
-    ):
-
-        output_file_name = str(Path(output_dir) / f"{image_name}.ome.tiff")
-
-        self._prepare_image_info(
-            image_name,
-            reg_transform_seq=self.reg_transform_seq,
-            write_pyramid=write_pyramid,
-            tile_size=tile_size,
-            compression=compression,
-            by_tile=True,
-        )
-
-        # create per tile coordinate information for fixed
-        # and moving
-        tile_coordinate_data = generate_tile_coords(
-            (self.x_size, self.y_size),
-            (self.PhysicalSizeX, self.PhysicalSizeY),
-            tile_size=self.tile_size,
-        )
-        # itk_transforms = [
-        #     t.itk_transform for t in self.reg_transform_seq.reg_transforms
-        # ]
-
-        tile_coordinate_data = add_tile_location_on_moving(
-            self.reg_transform_seq.reg_transforms_itk_order,
-            tile_coordinate_data,
-            self.reg_image.image_res,
-        )
-
-        if tile_padding is None:
-            tile_padding = determine_moving_tile_padding(
-                self.reg_transform_seq.reg_transforms_itk_order
-            )
-
-        print(f"saving to {output_file_name}")
-        with TiffWriter(output_file_name, bigtiff=True) as tif:
-            options = dict(
-                tile=(self.tile_size, self.tile_size),
-                compression=self.compression,
-                photometric="rgb" if self.reg_image.is_rgb else "minisblack",
-                metadata=None,
-            )
-            if self.reg_image.is_rgb:
-                resampled_zarray = itk_transform_tiles(
-                    self.reg_image,
-                    self.y_size,
-                    self.x_size,
-                    tile_coordinate_data,
-                    self.reg_transform_seq.composite_transform,
-                    tile_size=self.tile_size,
-                    tile_padding=tile_padding,
-                    use_multiprocessing=use_multiprocessing,
-                )
-                print(
-                    f"writing base layer RGB - shape: {resampled_zarray.shape}"
-                )
-                description = self.omexml
-                tif.write(
-                    zarr_to_tiles(
-                        resampled_zarray,
-                        tile_coordinate_data,
-                        self.reg_image.is_rgb,
-                    ),
-                    subifds=self.subifds,
-                    description=description,
-                    shape=(self.y_size, self.x_size, 3),
-                    dtype=resampled_zarray.dtype,
-                    **options,
-                )
-                if write_pyramid:
-                    for pyr_idx in range(1, self.n_pyr_levels):
-
-                        resampled_zarray_subres, orig_shape = compute_sub_res(
-                            self.reg_image,
-                            resampled_zarray,
-                            2**pyr_idx,
-                            self.tile_size,
-                            self.reg_image.is_rgb,
-                        )
-                        print(f"pyr {pyr_idx} : RGB-shape: {orig_shape}")
-
-                        tif.write(
-                            subres_zarr_to_tiles(
-                                resampled_zarray_subres,
-                                self.tile_size,
-                                self.reg_image.is_rgb,
-                            ),
-                            shape=orig_shape,
-                            dtype=resampled_zarray_subres.dtype,
-                            **options,
-                            subfiletype=1,
-                        )
-                try:
-                    resampled_zarray.store.clear()
-                except FileNotFoundError:
-                    pass
-            else:
-                for channel_idx in range(self.reg_image.n_ch):
-                    resampled_zarray = itk_transform_tiles(
-                        self.reg_image,
-                        self.y_size,
-                        self.x_size,
-                        tile_coordinate_data,
-                        self.reg_transform_seq.composite_transform,
-                        ch_idx=channel_idx,
-                        tile_size=self.tile_size,
-                        tile_padding=tile_padding,
-                        use_multiprocessing=use_multiprocessing,
-                    )
-
-                    description = self.omexml if channel_idx == 0 else None
-                    print(
-                        f"writing channel {channel_idx} - shape: {resampled_zarray.shape}"
-                    )
-                    tif.write(
-                        zarr_to_tiles(
-                            resampled_zarray,
-                            tile_coordinate_data,
-                            self.reg_image.is_rgb,
-                        ),
-                        subifds=self.subifds,
-                        description=description,
-                        shape=(self.y_size, self.x_size),
-                        dtype=resampled_zarray.dtype,
-                        **options,
-                    )
-                    if write_pyramid:
-                        for pyr_idx in range(1, self.n_pyr_levels):
-                            (
-                                resampled_zarray_subres,
-                                orig_shape,
-                            ) = compute_sub_res(
-                                self.reg_image,
-                                resampled_zarray,
-                                2**pyr_idx,
-                                self.tile_size,
-                                self.reg_image.is_rgb,
-                            )
-
-                            tif.write(
-                                subres_zarr_to_tiles(
-                                    resampled_zarray_subres,
-                                    self.tile_size,
-                                    self.reg_image.is_rgb,
-                                ),
-                                shape=orig_shape,
-                                dtype=resampled_zarray_subres.dtype,
-                                **options,
-                                subfiletype=1,
-                            )
-                    try:
-                        resampled_zarray.store.clear()
-                    except FileNotFoundError:
-                        pass
         return output_file_name
