0df9ee42ea784c8370cb53a6a9fbae6535c94eab
nhpatterson <nheathpatterson@gmail.com>
2022-08-24 09:19:43 -0500

    add 2d translation conversion from elastix to SimpleITK, black formatting

--- wsireg/utils/tform_conversion.py
+++ wsireg/utils/tform_conversion.py
@@ -3,12 +3,18 @@ from copy import deepcopy
 import SimpleITK as sitk
 
 
-def euler_elx_to_itk2d(tform):
+def euler_elx_to_itk2d(tform, is_translation=False):
     euler2d = sitk.Euler2DTransform()
 
-    center = [float(p) for p in tform['CenterOfRotationPoint']]
-    euler2d.SetFixedParameters(center)
-    elx_parameters = [float(p) for p in tform['TransformParameters']]
+    if is_translation:
+        elx_parameters = [0]
+        elx_parameters_trans = [float(p) for p in tform['TransformParameters']]
+        elx_parameters.extend(elx_parameters_trans)
+    else:
+        center = [float(p) for p in tform['CenterOfRotationPoint']]
+        euler2d.SetFixedParameters(center)
+        elx_parameters = [float(p) for p in tform['TransformParameters']]
+
     euler2d.SetParameters(elx_parameters)
 
     return euler2d
@@ -66,6 +72,8 @@ def convert_to_itk(tform):
         itk_tform = affine_elx_to_itk2d(tform)
     elif tform["Transform"][0] == "SimilarityTransform":
         itk_tform = similarity_elx_to_itk2d(tform)
+    elif tform["Transform"][0] == "TranslationTransform":
+        itk_tform = euler_elx_to_itk2d(tform, is_translation=True)
     elif tform["Transform"][0] == "EulerTransform":
         itk_tform = euler_elx_to_itk2d(tform)
     elif tform["Transform"][0] == "BSplineTransform":
